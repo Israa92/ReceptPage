fc61f483ac5f2fbab978bc7c3f70cbcb6da11aae
israa92 <israa.alazam@yahoo.se>
2016-01-12 22:14:37 +0100

    all basics are done

---
+++ src/ReceptPage/Models/Recept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReceptPage.Models
+{
+    public class Recept
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Ingredients { get; set; }
+        public string HowToDo { get; set; }
+    }
+}
